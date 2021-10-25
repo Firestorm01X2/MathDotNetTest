e91765989b7f926fe3ac8a8cc55b8e2e2195225a
firestorm01x2 <firestorm01x2gmail.com>
2021-10-25 14:40:18 +0300

    Initial commit

---
+++ MathDotNetTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using MathNet.Numerics;
+using MathNet.Numerics.Integration;
+using MathNet.Symbolics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathDotNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double,double> f= SymbolicExpression.Parse("x*x").Compile("x");
+            Console.WriteLine(f(3));//=9
+            double composite = SimpsonRule.IntegrateComposite(f, 0.0, 100, 1000);
+            Console.WriteLine(composite);//=333333.333333
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MathDotNetTest
+Func<double,double> f= SymbolicExpression.Parse("x*x").Compile("x");
+Console.WriteLine(f(3));//=9
+double composite = SimpsonRule.IntegrateComposite(f, 0.0, 100, 1000);
+Console.WriteLine(composite);//=333333.333333
